4f12085e8b3c9e231016ae0864ae50b1a3a952be
Alagu Paramesh Veerappan <alaguparamesh10@gmail.com>
2016-05-14 00:50:06 -0400

    Client update iteration 1

--- SYSC3303PROJECT/src/TFTPClient.java
+++ SYSC3303PROJECT/src/TFTPClient.java
@@ -1,4 +1,3 @@
-
 // TFTPClient.java
 // This class is the client side for a very simple assignment based on TFTP on
 // UDP/IP. The client uses one port and sends a read or write request and gets 
@@ -8,288 +7,230 @@ import java.io.*;
 import java.net.*;
 import java.util.*;
 
-
 public class TFTPClient {
+	public static final int DATA_SIZE = 512;
+	public static final int TOTAL_SIZE = DATA_SIZE +4;
+	public static enum Request { READ, WRITE, ERROR};
+	public static enum Mode { NORMAL, TEST};
+	public Request req;
+	private String filename;
+	private String mode;
+	
+	private DatagramPacket sendPacket, receivePacket, ack;
+	private DatagramSocket sendReceiveSocket;
 
-   private DatagramPacket sendPacket, receivePacket, ack;
-   private DatagramSocket sendReceiveSocket;
-   public static enum Request { READ, WRITE, ERROR};
-   public static Request req;
-   private static String filename;
-   public static final byte[] readResp = {0, 3, 0, 1};
-   public static final byte[] writeResp = {0, 4, 0, 0};
-   
-   // we can run in normal (send directly to server) or test
-   // (send to simulator) mode
-   public static enum Mode { NORMAL, TEST};
-
-   public TFTPClient()
-   {
-      try {
-         // Construct a datagram socket and bind it to any available
-         // port on the local host machine. This socket will be used to
-         // send and receive UDP Datagram packets.
-         sendReceiveSocket = new DatagramSocket();
-      } catch (SocketException se) {   // Can't create the socket.
-         se.printStackTrace();
-         System.exit(1);
-      }
-   }
-
-   public void sendAndReceive() throws FileNotFoundException, IOException
-   {
-	   int blockNum = 0;
-		byte[] set = new byte[100];
-		byte[] fn = new byte[filename.length()];
-		
-		if(req==Request.READ)
-
-		{
-			
-			set[0] = 0;
-	        set[1] = 1;
-	        set[2] = 0;
-	        set[3] = 0; //setting opcode
-	        fn = filename.getBytes();
-	        System.arraycopy(fn,0,set,4,fn.length);
-	        int i = fn.length+4;
-	        set[i] = 0;
-	        String mode = "octet";
-	        byte[] md = mode.getBytes();
-	        // and copy into the msg
-	        System.arraycopy(md,0,set,i+1,md.length);
-	        set[i+md.length+1] = 0;
-	        
-	        for(int o = 0 ; o<i+md.length+2;o++){
-	        	System.out.println(set[o]);
-	        }
-	        
-	        try {
-	            sendPacket = new DatagramPacket(set, i+md.length+2,
-	                                InetAddress.getLocalHost(), 23);
-	         } catch (UnknownHostException e) {
-	            e.printStackTrace();
-	            System.exit(1);
-	         } //send opcode to server
-	        
-	        try {
-		           // Block until a datagram is received via sendReceiveSocket.
-		           sendReceiveSocket.send(sendPacket);
-		        } catch(IOException e) {
-		           e.printStackTrace();
-		           System.exit(1);
-		        } //get our data from the server
-	        
-			 	byte [] data = new byte[516];
-		        receivePacket = new DatagramPacket(data, data.length);
-
-		        System.out.println("Client: Waiting for packet.");
-		        try {
-		           // Block until a datagram is received via sendReceiveSocket.
-		           sendReceiveSocket.receive(receivePacket);
-		        } catch(IOException e) {
-		           e.printStackTrace();
-		           System.exit(1);
-		        } //get our data from the server
-		        
-		        for(;;)
-		        {
-		            
-		             // if the client reads, it will send an acknowledge
-		            
-		            // if the client writes, it will send data depends on ack
-		            /* Read the file in 512 byte chunks. */
-
-		            BufferedOutputStream out =
-		                    new BufferedOutputStream(new FileOutputStream("TESTREADCLIENT.dat"));
-		            
-		          //  while ((n = in.read(data)) != -1) {
-		                /* 
-		                 * We just read "n" bytes into array data. 
-		                 * Now write them to the output file. 
-		                 */ //specify the name of file
-		                   out.write(data, 4, data.length);
-		        //}
-		            
-		            set[0] = 0;
-	            	set[1] = 3; //get ack ready
-	            	set[2] = data[2];
-		            //ack sending
-		            set[3]  = data[3];
-		            
-		            try {
-		                sendPacket = new DatagramPacket(set, set.length,
-		                                    InetAddress.getLocalHost(), 23);
-		             } catch (UnknownHostException e) {
-		                e.printStackTrace();
-		                System.exit(1);
-		             }
-		            
-		            if(data.length != 516)
-		            {
-		            	
-		            	break;
-		            }
-		            
-		            System.out.println("Client: Waiting for packet.");
-			        try {
-			           // Block until a datagram is received via sendReceiveSocket.
-			           sendReceiveSocket.receive(receivePacket);
-			        } catch(IOException e) {
-			           e.printStackTrace();
-			           System.exit(1);
-			        }
+   	public TFTPClient()
+   	{
+		try {
+			// Construct a datagram socket and bind it to any available
+			// port on the local host machine. This socket will be used to
+			// send and receive UDP Datagram packets.
+			sendReceiveSocket = new DatagramSocket();
+		} catch (SocketException se) {   // Can't create the socket.
+			se.printStackTrace();
+			System.exit(1);
 		}
-		
 	}
 
+   	public void run(){
+		Scanner re = new Scanner(System.in);
+		int cmd = 0;
+      
+		for(;;){
+			while(true){
+				try{
+					System.out.print("Please pick (1) read or (2) write or (5) shutdown: ");
+					cmd = Integer.parseInt(re.nextLine());
+					if(cmd == 1) {
+						req = Request.READ;
+						break;
+					} else if(cmd == 2) {
+						req = Request.WRITE;
+						break;
+					} else if (cmd == 5){
+						System.exit(1);
+					}
+				} catch(NumberFormatException e) {
+					System.out.println("Please enter a valid option");
+				}
+			}
+			
+			while(true){
+				try{
+					System.out.print("Please enter filename that you want to: ");
+					filename = "clientSide/"+re.nextLine();
+					File input = new File(filename);
+					Scanner read = new Scanner(input);
+					break;
+				} catch(FileNotFoundException e) {
+					System.out.println("Please enter a valid filename");
+				}
+			}
+			while(true){
+				try{
+					System.out.print("Please pick (1) netascii or (2) octet: ");
+					cmd = Integer.parseInt(re.nextLine());
+					if(cmd == 1) {
+						mode = "netascii";
+						break;
+					} else if(cmd == 2) {
+						mode = "octet";
+						break;
+					}
+				} catch(NumberFormatException e) {
+					System.out.println("Please enter a valid option");
+				}
+			}
+			byte[] msg = new byte[TOTAL_SIZE];
+			
+			msg[0] = 0;
+			if(req==Request.READ) {
+				msg[1] = 1;
+				
+			} else if(req==Request.WRITE) {
+				msg[1] = 2;
+			}
+			int index = 2;
+			
+			byte[] fn = filename.getBytes();
+			System.arraycopy(fn,0,msg,index,fn.length);
+			index = index + fn.length;
+			msg[index] = 0;
+			index++;
+			
+			byte[] md = mode.getBytes();
+			System.arraycopy(md,0,msg,index,md.length);
+			index = index + md.length;
+			msg[index] = 0;
+			index++;
 
-		else if(req==Request.WRITE){
-			set[0]=0;
-			set[1]=2;
-			set[2]=0;
-			set[3]=0; // setting
-																		// opcode
-		fn=filename.getBytes();
-		System.arraycopy(fn,0,set,4,fn.length);
-
-		int i = fn.length + 4;
-		set[i]=0;
-		String mode = "octet";
-		byte[] md = mode.getBytes();
-		// and copy into the msg
-		System.arraycopy(md,0,set,i+1,md.length);
-		set[i+md.length+1]=0;
-
-		try
-
-		{
-			sendPacket = new DatagramPacket(set, i+md.length+2, InetAddress.getLocalHost(), 23);
-		} catch(UnknownHostException e)
-
-		{
-			e.printStackTrace();
-			System.exit(1);
-		} // send opcode to server
-
-		try {
-	           // Block until a datagram is received via sendReceiveSocket.
-	           sendReceiveSocket.send(sendPacket);
-	        } catch(IOException e) {
-	           e.printStackTrace();
-	           System.exit(1);
-	        } //get our data from the server
-     
-		byte [] data = new byte[516];
-        receivePacket = new DatagramPacket(data, data.length);
-
-        System.out.println("Client: Waiting for packet.");
-        try {
-           // Block until a datagram is received via sendReceiveSocket.
-           sendReceiveSocket.receive(receivePacket);
-        } catch(IOException e) {
-           e.printStackTrace();
-           System.exit(1);
-        } //get our data from the server
-		
-		int j;
-		int len = data.length;
-		for(j=2;j<len;j++)
-
-		{
-			if (data[j] == 0)
-				break;
-		}
-		// extract filename
-		filename=new String(data,2,j-2);
-
-		// prepare file for transfer to client.
-		BufferedInputStream in;
-		in=new BufferedInputStream(new FileInputStream("readTest.dat"));
-
-		byte[] data1= new byte[516];
-		int n;
-
-		// construct datagram packet that is to
-		// be sent to a specified port on a specified host.
-
-		for(int k = 0;k<4;k++) // initiliaze opcode+block number
-		data1[k]=readResp[k];
-
-		while((n=in.read(data1))!=1)
-
-		{// if data.length()<512, last packet
-
-			// ack = new DatagramPacket(ackByte, ackByte.length);
-			sendPacket = new DatagramPacket(data1, data1.length, receivePacket.getAddress(), receivePacket.getPort());
-
-			sendReceiveSocket.send(sendPacket);
-			sendReceiveSocket.receive(ack);
-
-			// if data[3] is at max, reset to zero and increase data[2] by 1 (block
-			// number counter)
-			if ((data1[3]++) == 256) {
-				data1[2]++;
-				data1[3] = 0;
+			try {
+				sendPacket = new DatagramPacket(msg, index,
+								InetAddress.getLocalHost(), 23);
+			} catch (UnknownHostException e) {
+				e.printStackTrace();
+				System.exit(1);
+			}
+			
+			if(req==Request.READ) {
+				read();
+			} else if(req==Request.WRITE) {
+				write();
 			}
 		} 
-		in.close();
-		sendReceiveSocket.close();
-
 	}
-   }
-
-   public static void main(String args[])
-   {
-      TFTPClient c = new TFTPClient();
-      Scanner re = new Scanner(System.in);
-      boolean error = true;
-      int cmd = 0;
-      
-      for(;;){
-    	  if (cmd != 5){
-		      while(error){
-			      try{
-			    	  System.out.print("Please pick (1) read or (2) write or shutdown (5): ");
-			    	  cmd = Integer.parseInt(re.nextLine());
-			    	  if(cmd == 1){
-			    		  req = Request.READ;
-			    		  error = false;
-			    	  } else if(cmd == 2){
-			    		  req = Request.WRITE;
-			    		  error = false;
-			    	  } else if (cmd == 5){
-			    		  error = false;
-			    	  }
-			      } catch(NumberFormatException e){
-			    	  System.out.println("Error");
-			      }
-		      }
-		      error = true;
-		           
-		      while(error){
-			      try{
-			    	  System.out.print("Please enter filename that you want to: ");
-			    	  filename = re.nextLine();
-			    	  File input = new File(filename);
-			    	  Scanner read = new Scanner(input);
-			    	  error = false;
-			      } catch(FileNotFoundException e){
-			    	  System.out.println("Error");
-			      }
-		      }
-		      
-		      try {
-				c.sendAndReceive();
-			} catch (FileNotFoundException e) {
-				// TODO Auto-generated catch block
-				e.printStackTrace();
-			} catch (IOException e) {
-				// TODO Auto-generated catch block
-				e.printStackTrace();
+   	public void read() {
+   		byte blocknum1=0;
+   		byte blocknum2=0;
+   		try{
+   			BufferedOutputStream out = new BufferedOutputStream(new FileOutputStream("TESTREADCLIENT.dat"));
+   			for(;;){
+                		int len;
+                		byte[] msg = new byte[TOTAL_SIZE];
+                		byte[] data = new byte[DATA_SIZE];
+                
+		                DatagramPacket received = new DatagramPacket(msg, msg.length);
+		                try{
+		                	sendReceiveSocket.receive(received);
+		                } catch (IOException e){
+		                	e.printStackTrace();
+		                    System.exit(1);
+                		}
+                
+		                System.arraycopy(received.getData(), 4, data, 0, received.getLength()-4);
+                
+		                for(len = 4; len < data.length; len++) {
+		                    if (data[len] == 0) break;
+		                }
+		                out.write(data,0,data.length);
+		                System.out.println("Sending ack");
+                
+		                byte[] ack = new byte[4];
+		                ack[0] = 0;
+		                ack[1] = 4;
+		                ack[2] = blocknum1;
+		                ack[3] = blocknum2;
+		                
+		                DatagramPacket sending = new DatagramPacket(ack, ack.length,
+								InetAddress.getLocalHost(), 23);
+		                try{
+		                	sendReceiveSocket.send(sending);
+		                } catch (IOException e){
+		                	e.printStackTrace();
+		                    System.exit(1);
+		                }
+		                 
+		                if(len+1<=TOTAL_SIZE) {
+		                    out.close();
+		                    break;
+		                }
+		                 
+		                if(blocknum2 == 255){
+		                	blocknum1++;
+		                }
+		                blocknum2++;
 			}
-		  } 
-    	  break;
-      }
-   }
+   		} catch (IOException e){
+   			e.printStackTrace();
+            System.exit(1);
+   		}
+	}
+   	
+   	public void write() {
+   		byte blocknum1=0;
+   		byte blocknum2=0;
+   		int len;
+   		try{
+   			BufferedInputStream in = new BufferedInputStream(new FileInputStream(filename));
+   			do{
+		                byte[] msg = new byte[TOTAL_SIZE];
+		                byte[] data = new byte[DATA_SIZE];
+		                
+		                DatagramPacket received = new DatagramPacket(msg, msg.length);
+		                try{
+		                	sendReceiveSocket.receive(received);
+		                } catch (IOException e){
+		                	e.printStackTrace();
+		                    System.exit(1);
+		                }
+		                
+		                if(blocknum2 == 255){
+		                	blocknum1++;
+		                }
+		                blocknum2++;
+		                
+		                len = in.read(data);
+		                                
+		                byte[] send = new byte[TOTAL_SIZE];
+		                send[0] = 0;
+		                send[1] = 3;
+		                send[2] = blocknum1;
+		                send[3] = blocknum2;
+		                System.arraycopy(data, 0, send, 4, len);
+		                DatagramPacket sending = new DatagramPacket(send, send.length,
+								InetAddress.getLocalHost(), 23);
+		                try{
+		                	sendReceiveSocket.send(sending);
+		                } catch (IOException e){
+		                	e.printStackTrace();
+		                    System.exit(1);
+		                }                 
+		 	} while (len>=DATA_SIZE);
+   			in.close();
+   		} catch (FileNotFoundException e){
+   			e.printStackTrace();
+   			System.exit(1);
+   		} catch (IOException e){
+   			e.printStackTrace();
+   			System.exit(1);
+   		}
+	}
+	
+	public static void main(String args[])
+	{
+		TFTPClient c = new TFTPClient();
+		for(;;){
+			c.run();
+		}
+	}
 }
