34e3d6232838cbc30be2544cd83f003bdc3ddd8d
Alagu Paramesh Veerappan <alaguparamesh10@gmail.com>
2016-05-16 20:15:06 -0400

    Update TFTPClient.java

--- SYSC3303PROJECT/src/TFTPClient.java
+++ SYSC3303PROJECT/src/TFTPClient.java
@@ -21,25 +21,23 @@ public class TFTPClient {
 
    	public TFTPClient()
    	{
-		try {
-			// Construct a datagram socket and bind it to any available
-			// port on the local host machine. This socket will be used to
-			// send and receive UDP Datagram packets.
+   		try {
 			sendReceiveSocket = new DatagramSocket();
-		} catch (SocketException se) {   // Can't create the socket.
+		} catch (SocketException se) {
 			se.printStackTrace();
 			System.exit(1);
 		}
 	}
 
-   	public void run(){
+   	public void run()
+   	{
 		Scanner re = new Scanner(System.in);
 		int cmd = 0;
-      
-		for(;;){
-			while(true){
-				try{
-					System.out.print("Please pick (1) read or (2) write or (5) shutdown: ");
+
+		for(;;) {
+			while(true) {
+				try {
+					System.out.print("[1]Read   [2]Write  [5]Shutdown : ");
 					cmd = Integer.parseInt(re.nextLine());
 					if(cmd == 1) {
 						req = Request.READ;
@@ -47,17 +45,17 @@ public class TFTPClient {
 					} else if(cmd == 2) {
 						req = Request.WRITE;
 						break;
-					} else if (cmd == 5){
+					} else if (cmd == 5) {
 						System.exit(1);
 					}
 				} catch(NumberFormatException e) {
 					System.out.println("Please enter a valid option");
 				}
 			}
-			
-			while(true){
-				try{
-					System.out.print("Please enter filename that you want to: ");
+
+			while(true) {
+				try {
+					System.out.print("Filename : ");
 					filename = re.nextLine();
 					File input = new File(filename);
 					Scanner read = new Scanner(input);
@@ -66,9 +64,9 @@ public class TFTPClient {
 					System.out.println("Please enter a valid filename");
 				}
 			}
-			/*/while(true){
-				try{
-					System.out.print("Please pick (1) netascii or (2) octet: ");
+			while(true) {
+				try {
+					System.out.print("[1]netascii  [2]octet : ");
 					cmd = Integer.parseInt(re.nextLine());
 					if(cmd == 1) {
 						mode = "netascii";
@@ -80,7 +78,7 @@ public class TFTPClient {
 				} catch(NumberFormatException e) {
 					System.out.println("Please enter a valid option");
 				}
-			}*/
+			}
 			mode = "octet";
 			byte[] msg = new byte[TOTAL_SIZE];
 			
@@ -106,8 +104,7 @@ public class TFTPClient {
 			index++;
 
 			try {
-				sendPacket = new DatagramPacket(msg, index,
-								InetAddress.getLocalHost(), 23);
+				sendPacket = new DatagramPacket(msg, index, InetAddress.getLocalHost(), 23);
 			} catch (UnknownHostException e) {
 				e.printStackTrace();
 				System.exit(1);
@@ -115,7 +112,7 @@ public class TFTPClient {
 			
 			try{
             	sendReceiveSocket.send(sendPacket);
-            } catch (IOException e){
+            } catch (IOException e) {
             	e.printStackTrace();
                 System.exit(1);
             }
@@ -127,128 +124,135 @@ public class TFTPClient {
 			}
 		} 
 	}
-   	public void read() {
-   		byte blocknum1=0;
-   		byte blocknum2=1;
-   		try{
-   			BufferedOutputStream out = new BufferedOutputStream(new FileOutputStream("read.txt"));
-   			//for(;;){
-                		int len;
-                		byte[] msg = new byte[TOTAL_SIZE];
-                		byte[] data = new byte[DATA_SIZE];
+   	
+   	public void read()
+   	{
+		byte blocknum1=0;
+		byte blocknum2=1;
+		try {
+			BufferedOutputStream out = new BufferedOutputStream(new FileOutputStream("read.txt"));
+			for(;;) {
+				int len;
+				byte[] msg = new byte[TOTAL_SIZE];
+				byte[] data = new byte[DATA_SIZE];
+
+				receivePacket = new DatagramPacket(msg, msg.length);
+
+				try {
+					sendReceiveSocket.receive(receivePacket);
+				} catch (IOException e) {
+					e.printStackTrace();
+					System.exit(1);
+				}
                 
-		                receivePacket = new DatagramPacket(msg, msg.length);
-		                try{
-		                	sendReceiveSocket.receive(receivePacket);
-		                } catch (IOException e){
-		                	e.printStackTrace();
-		                    System.exit(1);
-                		}
-                
-		                System.arraycopy(receivePacket.getData(), 4, data, 0, receivePacket.getLength()-4);
-                System.out.print("test");
-		                for(len = 4; len < data.length; len++) {
-		                    if (data[len] == 0) break;
-		                }
-		                out.write(data,0,len);
-		                System.out.println("Sending ack");
-                
-		                byte[] ack = new byte[4];
-		                ack[0] = 0;
-		                ack[1] = 4;
-		                ack[2] = blocknum1;
-		                ack[3] = blocknum2;
-		                
-		                sendPacket = new DatagramPacket(ack, ack.length,
-								InetAddress.getLocalHost(), 23);
-		                try{
-		                	sendReceiveSocket.send(sendPacket);
-		                } catch (IOException e){
-		                	e.printStackTrace();
-		                    System.exit(1);
-		                }
+				System.arraycopy(receivePacket.getData(), 4, data, 0, receivePacket.getLength()-4);
+
+				for(len = 4; len < data.length; len++) {
+					if (data[len] == 0) break;
+				}
+
+				out.write(data,0,len);
+				System.out.println("Sending ack");
+
+				byte[] ack = new byte[4];
+				ack[0] = 0;
+				ack[1] = 4;
+				ack[2] = blocknum1;
+				ack[3] = blocknum2;
+
+				sendPacket = new DatagramPacket(ack, ack.length, InetAddress.getLocalHost(), 23);
+
+				try {
+					sendReceiveSocket.send(sendPacket);
+				} catch (IOException e) {
+					e.printStackTrace();
+					System.exit(1);
+				}
 		                 
-		                if(len+1<=TOTAL_SIZE) {
-		                    out.close();
-		                    //break;
-		                }
-		                 
-		                if(blocknum2 == 255){
-		                	blocknum1++;
-		                }
-		                blocknum2++;
-			//}
-   		} catch (IOException e){
-   			e.printStackTrace();
-            System.exit(1);
-   		}
+				if(len+1<=TOTAL_SIZE) {
+					out.close();
+					break;
+				}
+
+				if(blocknum2 == 255) {
+					blocknum1++;
+				}
+				blocknum2++;
+			}
+		} catch (IOException e) {
+			e.printStackTrace();
+			`System.exit(1);
+		}
 	}
    	
-   	public void write() {
-   		byte blocknum1=0;
-   		byte blocknum2=1;
-   		int len;
-   		try{
-   			BufferedInputStream in = new BufferedInputStream(new FileInputStream("file.txt"));
-   			do{
-		                byte[] msg = new byte[TOTAL_SIZE];
-		                byte[] data = new byte[DATA_SIZE];
-		                
-		                receivePacket = new DatagramPacket(msg, msg.length);
-		                try{
-		                	sendReceiveSocket.receive(receivePacket);
-		                } catch (IOException e){
-		                	e.printStackTrace();
-		                    System.exit(1);
-		                }
-		                
-		                if(blocknum2 == 255){
-		                	blocknum1++;
-		                }
-		                blocknum2++;
-		                
-		                len = in.read(data);
-		                
-		                msg[0] = 0;
-		                msg[1] = 3;
-		                msg[2] = blocknum1;
-		                msg[3] = blocknum2;
-		                
-		                int i = 0; 
-		                for(;;){
-		                	System.arraycopy(data, i, msg, i+4, len);
-		                	if(data[i]==0){
-		                		break;
-		                	} else {
-		                		i++;
-		                	}
-		                }
-		                data[i] = 0;
-		                i++;
-		                
-		                sendPacket = new DatagramPacket(msg, i+4, InetAddress.getLocalHost(), 23);
-		                try{
-		                	sendReceiveSocket.send(sendPacket);
-		                } catch (IOException e){
-		                	e.printStackTrace();
-		                    System.exit(1);
-		                }
-		              
-		 	} while (len>=DATA_SIZE);
-   			in.close();
-   		} catch (FileNotFoundException e){
-   			e.printStackTrace();
-   			System.exit(1);
-   		} catch (IOException e){
-   			e.printStackTrace();
-   			System.exit(1);
-   		}
+	public void write() {
+		byte blocknum1=0;
+		byte blocknum2=1;
+		int len;
+		try {
+			BufferedInputStream in = new BufferedInputStream(new FileInputStream("file.txt"));
+			do {
+				byte[] msg = new byte[TOTAL_SIZE];
+				byte[] data = new byte[DATA_SIZE];
+
+				receivePacket = new DatagramPacket(msg, msg.length);
+
+				try {
+					sendReceiveSocket.receive(receivePacket);
+				} catch (IOException e) {
+					e.printStackTrace();
+					System.exit(1);
+				}
+				
+				if(blocknum2 == 255) {
+					blocknum1++;
+				}
+				blocknum2++;
+				
+				len = in.read(data);
+				System.out.println(len);
+				
+				msg[0] = 0;
+				msg[1] = 3;
+				msg[2] = blocknum1;
+				msg[3] = blocknum2;
+
+				int i = 0; 
+				for(;;) {
+					System.arraycopy(data, i, msg, i+4, len-1);
+					if(data[i]==0) {
+						break;
+					} else {
+						i++;
+					}
+				}
+				data[i] = 0;
+				i++;
+				
+				sendPacket = new DatagramPacket(msg, i+4, InetAddress.getLocalHost(), 23);
+				
+				try {
+					sendReceiveSocket.send(sendPacket);
+				} catch (IOException e){
+					e.printStackTrace();
+					System.exit(1);
+				}
+		
+		} while (len>=DATA_SIZE);
+			in.close();
+		} catch (FileNotFoundException e) {
+			e.printStackTrace();
+			System.exit(1);
+		} catch (IOException e) {
+			e.printStackTrace();
+			System.exit(1);
+		}
 	}
-	
+
 	public static void main(String args[])
 	{
 		TFTPClient c = new TFTPClient();
-		for(;;){
+		for(;;) {
 			c.run();
 		}
 	}
